1e181694c2cf1c4d7dc66b7d084875e0d63348a8
lexei <alex-iv999.ivanov@yandex.ru>
2022-05-17 20:48:54 +0300

    Project init Home Page

---
+++ YoungMan/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YoungMan.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
